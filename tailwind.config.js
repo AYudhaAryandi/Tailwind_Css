d497e5ddd98f82ef025054ca27624cb2d1090a71
AYudhaAryandi <yudhaaryandi03@gmail.com>
2023-12-01 11:03:57 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: { 
+    extend: {
+
+      // note : masukkan ke input css dulu
+      fontFamily:{
+        cek:['Anton'],
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
